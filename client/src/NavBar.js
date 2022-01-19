90f0737d6df24e27b31ea5cd5fa3f0dcfbb72eef
Kidus Yohannes <95446310+kidussyohanness@users.noreply.github.com>
2022-01-19 04:13:24 +0300

    Added new files

---
+++ client/src/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+const NavBar = (props) => {
+    return(
+        <div>
+            <button>Home</button>
+            <button>My List</button>
+            <button>Friends</button>
+            <button>About us</button>
+            <button>Contact Us</button>
+        </div>
+    );
+};
+
+export default NavBar;
